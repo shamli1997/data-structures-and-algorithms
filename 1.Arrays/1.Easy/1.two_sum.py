b50d2577cee1374e5b1a2b8ffd4a07dd56987d77
shamli ingole <shamliingole@gmail.com>
2022-07-25 16:47:00 +0530

    Added 2 sum and validate subsequence

---
+++ 1.Arrays/1.Easy/1.two_sum.py
@@ -0,0 +1,44 @@
+# Two Sum
+# input : 
+    # array : [3, 5, -4, 8, 11, 1, -1, 6]
+    # target_sum = 10
+# output :
+    # [-1, 11]
+
+# time : o(n^2), space : o(n)
+def twoNumberSumBrute(array, targetSum):
+	for i in range(0,len(array)-1):
+		firstNum = array[i]
+		for j in range(i+1,len(array)):
+			secondNum = array[j]
+			if firstNum + secondNum == targetSum:
+				return[firstNum,secondNum]
+			
+	return []
+
+# time : o(n), space : o(n)
+def twoNumberSumOptimized(array, targetSum):
+    nums = {}
+    for num in array:
+        potentialMatch = targetSum - num
+        if potentialMatch in nums:
+            return[num,potentialMatch]
+        else:
+            nums[num]=True
+    return []
+
+# time : o(n log n), space : o(n)
+def twoNumberSum(array, targetSum):
+    array.sort()
+    left = 0
+    right = len(array) - 1
+    while left < right:
+        currentSum = array[left] + array[right]
+        if currentSum == targetSum:
+            return [array[left], array[right]]
+        elif currentSum < targetSum:
+            left += 1
+        elif currentSum > targetSum:
+            right -= 1
+    return []
+
---
+++ 1.Arrays/1.Easy/2.validate_subsequence.py
@@ -0,0 +1,19 @@
+# Validate Subsequence
+# subsequence is set of array in same order as they appear in array
+# input : 
+    # array : [5, 1, 22, 25, 6, -1, 8, 10]
+    # sequence : [1, 6, -1, 10]
+# output :
+    # true
+
+# time : o(n), space : o(1)
+def isValidSubsequence(array, sequence):
+    # Write your code here.
+    arrIdx = 0
+    seqIdx = 0
+    while arrIdx < len(array) and seqIdx < len(sequence):
+        if array[arrIdx] == sequence[seqIdx]:
+            seqIdx += 1
+        arrIdx += 1
+        
+    return seqIdx == len(sequence)
